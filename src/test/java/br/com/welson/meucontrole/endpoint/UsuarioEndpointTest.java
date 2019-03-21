0372f37dd640efbb42e77552df9c6895958ee491
Welson Teles <welsonlimawlsn@gmail.com>
2019-03-21 13:42:24 -0300

    Testes de usuario

--- src/test/java/br/com/welson/meucontrole/endpoint/UsuarioEndpointTest.java
+++ src/test/java/br/com/welson/meucontrole/endpoint/UsuarioEndpointTest.java
@@ -1,8 +1,6 @@
 package br.com.welson.meucontrole.endpoint;
 
 import br.com.welson.meucontrole.dto.UsuarioDTO;
-import br.com.welson.meucontrole.persistencia.modelos.AtivacaoConta;
-import br.com.welson.meucontrole.persistencia.modelos.Conta;
 import br.com.welson.meucontrole.persistencia.modelos.Usuario;
 import br.com.welson.meucontrole.persistencia.repositorios.AtivacaoContaRepositorio;
 import br.com.welson.meucontrole.persistencia.repositorios.ContaRepositorio;
@@ -23,6 +21,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Optional;
+
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @RunWith(SpringRunner.class)
@@ -32,6 +32,12 @@ public class UsuarioEndpointTest {
 
     @MockBean
     private UsuarioRepositorio usuarioRepositorio;
+
+    @MockBean
+    private AtivacaoContaRepositorio ativacaoContaRepositorio;
+
+    @MockBean
+    private ContaRepositorio contaRepositorio;
 
     @Autowired
     private TestRestTemplate testRestTemplate;
@@ -61,9 +67,97 @@ public class UsuarioEndpointTest {
     }
 
     @Test
-    public void criarUsuarioDeveRetornar201() {
-        ResponseEntity<String> exchange = testRestTemplate.exchange("/novo-usuario", HttpMethod.POST, new HttpEntity<>(usuarioDTO), String.class);
+    public void criarUsuarioDeveRetornarCreatedEObjetoUsuario() {
+        ResponseEntity<Usuario> exchange = testRestTemplate.exchange("/novo-usuario", HttpMethod.POST, new HttpEntity<>(usuarioDTO), Usuario.class);
         Assertions.assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        Assertions.assertThat(exchange.getBody()).isEqualTo(usuario);
+    }
+
+    @Test
+    public void criarUsuarioNomeNullDeveRetornarBadRequest() {
+        UsuarioDTO usuarioFaltandoNome = mockUsuarioDTO();
+        usuarioFaltandoNome.setNome(null);
+        tentandoCriarUsuarioComDadosIncompletos(usuarioFaltandoNome);
+    }
+
+    @Test
+    public void criarUsuarioNomeEmptyDeveRetornarBadRequest() {
+        UsuarioDTO usuarioFaltandoNome = mockUsuarioDTO();
+        usuarioFaltandoNome.setNome("");
+        tentandoCriarUsuarioComDadosIncompletos(usuarioFaltandoNome);
+    }
+
+    @Test
+    public void criarUsuarioNomeDeUsuarioNullDeveRetornarBadRequest() {
+        UsuarioDTO usuarioFaltandoNome = mockUsuarioDTO();
+        usuarioFaltandoNome.setUsuario(null);
+        tentandoCriarUsuarioComDadosIncompletos(usuarioFaltandoNome);
+    }
+
+    @Test
+    public void criarUsuarioNomeDeUsuarioEmptyDeveRetornarBadRequest() {
+        UsuarioDTO usuarioFaltandoNome = mockUsuarioDTO();
+        usuarioFaltandoNome.setUsuario("");
+        tentandoCriarUsuarioComDadosIncompletos(usuarioFaltandoNome);
+    }
+
+    @Test
+    public void criarUsuarioSobrenomeNullDeveRetornarBadRequest() {
+        UsuarioDTO usuarioFaltandoNome = mockUsuarioDTO();
+        usuarioFaltandoNome.setSobrenome(null);
+        tentandoCriarUsuarioComDadosIncompletos(usuarioFaltandoNome);
+    }
+
+    @Test
+    public void criarUsuarioSobrenomeEmptyDeveRetornarBadRequest() {
+        UsuarioDTO usuarioFaltandoNome = mockUsuarioDTO();
+        usuarioFaltandoNome.setSobrenome("");
+        tentandoCriarUsuarioComDadosIncompletos(usuarioFaltandoNome);
+    }
+
+    @Test
+    public void criarUsuarioSenhaNullDeveRetornarBadRequest() {
+        UsuarioDTO usuarioFaltandoNome = mockUsuarioDTO();
+        usuarioFaltandoNome.setSenha(null);
+        tentandoCriarUsuarioComDadosIncompletos(usuarioFaltandoNome);
+    }
+
+    @Test
+    public void criarUsuarioSenhaEmptyDeveRetornarBadRequest() {
+        UsuarioDTO usuarioFaltandoNome = mockUsuarioDTO();
+        usuarioFaltandoNome.setSenha("");
+        tentandoCriarUsuarioComDadosIncompletos(usuarioFaltandoNome);
+    }
+
+    @Test
+    public void criarUsuarioEmailNullDeveRetornarBadRequest() {
+        UsuarioDTO usuarioFaltandoNome = mockUsuarioDTO();
+        usuarioFaltandoNome.setEmail(null);
+        tentandoCriarUsuarioComDadosIncompletos(usuarioFaltandoNome);
+    }
+
+    @Test
+    public void criarUsuarioEmailEmptyDeveRetornarBadRequest() {
+        UsuarioDTO usuarioFaltandoNome = mockUsuarioDTO();
+        usuarioFaltandoNome.setEmail("");
+        tentandoCriarUsuarioComDadosIncompletos(usuarioFaltandoNome);
+    }
+
+    @Test
+    public void criarUsuarioEmailJaExistebteDeveRetornarBadRequest() {
+        BDDMockito.when(usuarioRepositorio.findByEmail(usuarioDTO.getEmail())).thenReturn(Optional.of(usuario));
+        tentandoCriarUsuarioComDadosIncompletos(usuarioDTO);
+    }
+
+    @Test
+    public void criarUsuarioUsuarioJaExistenteDeveRetornarBadRequest() {
+        BDDMockito.when(usuarioRepositorio.findByUsuario(usuarioDTO.getUsuario())).thenReturn(Optional.of(usuario));
+        tentandoCriarUsuarioComDadosIncompletos(usuarioDTO);
+    }
+
+    private void tentandoCriarUsuarioComDadosIncompletos(UsuarioDTO usuarioFaltandoNome) {
+        ResponseEntity<Usuario> exchange = testRestTemplate.exchange("/novo-usuario", HttpMethod.POST, new HttpEntity<>(usuarioFaltandoNome), Usuario.class);
+        Assertions.assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
     }
 
 }
